4e7c830403e7256249405cdbe7e3e2b1d27fd279
Brianllamar <ilikerobot@gmail.com>
2014-05-02 07:04:58 -0400

    finished c exercise5 using lcm from the ruby docs

--- exercise5.rb
+++ exercise5.rb
@@ -2,20 +2,13 @@
 # What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 
 # set the array to include 1 through 20
-# use the array to step through each number in a loop to if all 20 numbers can be divisible by num = i += 1
-# answer via yahoo answers is 232,792,560 
+# use the array(1..20) to step through each number in a loop to if all 20 numbers can be divisible by num = i += 1
+# the answer via yahoo answers is 232,792,560 
+#  print the Least common multiple
 
-# setting some variables
-count = 1
+sample_array = (20.downto(1)).to_a # create the array 1 to 20
 
-while true
-	num = 20 * count
+least_common_denominator = sample_array.inject(:lcm) # find the least common denominator using using the lcm method between each integer in the array
 
-	20.downto(1).each do |i| # create the first number and repeat 1...20
-		if num % i == 0 # if the j is divisible by i
-			num
-		end
-		count += 2
-	end
-end 
+puts least_common_denominator
 
