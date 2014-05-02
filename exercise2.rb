056684a2e595f47fdcfb3c93bca085d119a354f7
Brianllamar <ilikerobot@gmail.com>
2014-05-01 21:18:49 -0400

    add notes

--- exercise2.rb
+++ exercise2.rb
@@ -6,24 +6,25 @@
 # add fibo numbers to an array
 # find the sum of all even fibo numbers less than 4mil
 
-	def fibo(n)
-		return n if n < 2
-	  fib = [1,2]
-		n.times do
-			fib << fib[-1] + fib[-2]
+	def fibo(n) # method to create fibonacci number; uses the argument for the place in the fibonacci sequence
+		n if n < 2 # leaves out 1 and in the check since the n[-1] and n[-2] would return the 1 and 1 again
+	  fib = [1,2] # because of the above, the fib array already sets 1 and 2 in the first two places
+		n.times do # begins the loop for creating the fibonacci sequence
+			fib << fib[-1] + fib[-2] # adds the places prior to create the next number in the sequence
 		end	
-		return fib.last
+		fib.last # returns the final number in the sequence.
 	end
 
-	# puts fibo(31)
+	# puts fibo(31) # commented test to see if the sequence is returned
 
+	# setting some variabls
 	max = 4_000_000_000
 	i = 1
   sum = 0
 
-	while((x = fibo(i)) < max)
-	  sum += x if x % 2 == 0
-	  i += 1
+	while((x = fibo(i)) < max) # loop created to find the fibonacci number less than 4 mil
+	  sum += x if x % 2 == 0 # adds the fibonacci number to the array if even
+	  i += 1 # increase the place of the fibonacci sequence by 1
 	end
 
 	puts sum
